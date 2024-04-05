c50fe44236dd2b6c0936f69f260112212a646507
iwbkratos <iwbkratos@gmail.com>
2024-04-05 19:01:26 +0530

    modified list.html,list.css and created list.js

---
+++ list.js
@@ -0,0 +1,26 @@
+// document.getElementById("meta").style.display="none";
+
+// function cardcount()
+// {
+//  const e=document.getElementById("count-limit");
+//   const val=Number(e.value);
+//  let num=val
+//   console.log(val);
+//  demo(num);
+// }
+
+// function demo(num)
+// {
+//   let val=Math.ceil(20/num)
+
+//   while(val>0)
+//   {
+//            createPage();
+//            val--;
+//   }
+// }
+
+// function createPage()
+// {
+//     var  htmldoc=document.implementation.createHTMLDocument("new page");
+// }
